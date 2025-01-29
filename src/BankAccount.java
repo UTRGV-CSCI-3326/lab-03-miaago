f6a3c3b1303bfa9583d80ee3067124fb60af0683
Maya G <124105229+miaago@users.noreply.github.com>
2025-01-29 21:14:59 +0000

    First commit (wip)

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,23 @@
+public class BankAccount
+{
+    public static void main (String[] args)
+    {
+        float bobBalance = 2175.37f;
+        // bobBalance = bobBalance - 302.50f;
+        // bobBalance = bobBalance + 50.03f;
+        // bobBalance = bobBalance/2 + 20.00f;
+        // bobBalance--;
+        // bobBalance = bobBalance * 2;
+        // bobBalance++;
 
+        bobBalance -= 302.50f;
+        bobBalance += 50.03f;
+        bobBalance /= 2; 
+        bobBalance += 20.00f;
+        bobBalance--;
+        bobBalance *= 2;
+        bobBalance++;
+
+        System.out.println(bobBalance);
+    }
+}
